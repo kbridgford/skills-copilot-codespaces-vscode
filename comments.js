248f847e261efab35724c20fb20336eef60f1fcb
Kyle Bridgford <57777806+kbridgford@users.noreply.github.com>
2025-01-24 22:47:40 +0000

    Correct comment phrasing in comments.js

--- comments.js
+++ comments.js
@@ -1,10 +1,9 @@
-// Create a web server
+// Create web server
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
 var path = require('path');
 var comments = require('./comments.json');
-
 http.createServer(function (req, res) {
   var q = url.parse(req.url, true);
   var filename = '.' + q.pathname;
