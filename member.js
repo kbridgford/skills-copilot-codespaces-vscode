ae3440184a102ae0daa90117945c8439b5e9e725
Kyle Bridgford <57777806+kbridgford@users.noreply.github.com>
2025-01-24 22:38:15 +0000

    Add skillsMember function to manage skill data

---
+++ member.js
@@ -0,0 +1,30 @@
+function skillsMember() {
+    return {
+      "skills": [
+        {
+          "id": "1",
+          "name": "Skill 1",
+          "description": "This is the first skill",
+          "type": "skill",
+          "image": "https://via.placeholder.com/150",
+          "link": "https://www.example.com/skill1"
+        },
+        {
+          "id": "2",
+          "name": "Skill 2",
+          "description": "This is the second skill",
+          "type": "skill",
+          "image": "https://via.placeholder.com/150",
+          "link": "https://www.example.com/skill2"
+        },
+        {
+          "id": "3",
+          "name": "Skill 3",
+          "description": "This is the third skill",
+          "type": "skill",
+          "image": "https://via.placeholder.com/150",
+          "link": "https://www.example.com/skill3"
+        }
+      ]
+    }
+  }
